eb774e74d3650262c127b489be13949b58157a5c
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-21 01:49:20 +0700

    Update ub20.sh

--- ub20.sh
+++ ub20.sh
@@ -187,10 +187,9 @@ function install_xray() {
    rm -rf /www/xray_web
    mkdir -p /www/xray_web
 # / /  Xray Core Version new
-   uuid="1d1c1d94-6987-4658-a4dc-8821a30fe7e0"
    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.5.8
 # set uuid
-uuid=$(cat /proc/sys/kernel/random/uuid)
+uuid="1d1c1d94-6987-4658-a4dc-8821a30fe7e0"
 # xray config
 cat > /etc/xray/config.json << END
 {
