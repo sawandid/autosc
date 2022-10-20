33d26ce1d07e2e73e6eaccc12327bdfd474c6284
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-21 02:30:17 +0700

    Update ub20.sh

--- ub20.sh
+++ ub20.sh
@@ -734,8 +734,8 @@ echo -e "$green┴ ┴└─┘ ┴ └─┘└─┘└─┘┴└─┴┴  
 echo -e "[ ${red}INFO${NC} ] Autoscript xray vpn lite (multi port)"
 echo -e "[ ${red}INFO${NC} ] no licence script (free lifetime)"
 echo -e "[ ${red}INFO${NC} ] Make sure the internet is smooth when installing the script"
-echo -e "[ ${Green}MANUAL{Font} ${red}POINTING{NC} ] First connect your VPS IP to the Domain click Y/yes "
-echo -e "[ ${Green}AUTO{Font} ${red}POINTING${NC} ] do you not have a domain? please click N/not
+echo -e "[ ${red} MANUAL POINTING{NC} ] First connect your VPS IP to the Domain click Y/yes "
+echo -e "[ ${red}AUTO POINTING${NC} ] do you not have a domain? please click N/not"
   read -rp "CONTINUING TO INSTALL AUTOSCRIPT (y/n)? "  menu_num 
   case $menu_num in
   Y)
