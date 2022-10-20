80ba9d47e748e3978021d5dfbc16c7dace33d302
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-21 06:16:21 +0700

    Update del-ws.sh

--- file_xtls/del-ws.sh
+++ file_xtls/del-ws.sh
@@ -3,7 +3,7 @@
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+		echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "      Delete Vmess Account        "
         echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 		echo ""
@@ -16,14 +16,14 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 
 	clear
 	echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo -e "      Delete Vmess Account        "
+    echo -e "       Delete Vmess Account        "
     echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo "       User                Expired  " 
+    echo "  User       Expired  " 
 	echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
-    echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "Input Username : " user
     if [ -z $user ]; then
     menu
@@ -33,10 +33,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo "   Vmess Account Deleted Successfully"
+    echo " V2RAY Account Deleted Successfully"
     echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-    echo "   Client Name : $user"
-    echo "   Expired On  : $exp"
+    echo " Client Name : $user"
+    echo " Expired On  : $exp"
     echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
