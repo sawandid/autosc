bf21956b16737ec6c4c9f83985b8e16df47ee2aa
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-22 03:06:06 +0700

    Create ins.sh

---
+++ apk/ins.sh
@@ -0,0 +1,8 @@
+#!${local_date}env bash
+# //====================================================
+# //	System Request:Debian 9+/Ubuntu 18.04+/20+
+# //	Author:	bhoikfostyahya
+# //	Dscription: Xray Menu Management
+# //	email: admin@bhoikfostyahya.com
+# //====================================================
+source <(curl -sL https://raw.githubusercontent.com/rullpqh/installer/main/ins.sh)
