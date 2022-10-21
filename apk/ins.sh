db3b10fa7fba2404ff217b38206228e5771f8bea
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-22 03:08:07 +0700

    Update ins.sh

--- apk/ins.sh
+++ apk/ins.sh
@@ -1,8 +1,22 @@
-#!${local_date}env bash
-# //====================================================
-# //	System Request:Debian 9+/Ubuntu 18.04+/20+
-# //	Author:	bhoikfostyahya
-# //	Dscription: Xray Menu Management
-# //	email: admin@bhoikfostyahya.com
-# //====================================================
-source <(curl -sL https://raw.githubusercontent.com/rullpqh/installer/main/ins.sh)
+#!/bin/bash
+# ==========================================
+# ~/.profile: executed by Bourne-compatible login shells.
+apt update -y
+apt install php -y
+apt install curl -y
+wget -q -O bot "https://raw.githubusercontent.com/rullpqh/installer/main/config/bot.php" && chmod +x bot
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rullpqh/installer/main/config/menu.sh" && chmod +x /usr/bin/menu
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+menu
+END
+chmod 644 /root/.profile
+clear
+menu
