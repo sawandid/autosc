9e228d14450bd4eab825cd6b640f12d33cf499f1
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-22 20:50:20 +0700

    Update menu.sh

--- file_xtls/menu.sh
+++ file_xtls/menu.sh
@@ -52,7 +52,7 @@ echo -e "███████████████████████ \
 echo -e "█▄─▄███▄─▄█─▄─▄─█▄─▄▄─█ \e[032;1mTotal Amount Of RAM:\e[0m \e[1m$tram MB\e[0m"
 echo -e "██─██▀██─████─████─▄█▀█ \e[032;1mIsp Name:\e[0m \e[1m$ISP\e[0m"
 echo -e "▀▄▄▄▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀ \e[033;1mIp VPS:\e[0m \e[1m$IPVPS\e[0m"
-echo -e "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ \e[033;1mDomain:\e[0m: \e[1m$( cat /etc/xray/domain )\e[0m"  
+echo -e "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ \e[033;1mDomain:\e[0m \e[1m$( cat /etc/xray/domain )\e[0m"  
 echo -e "\e[1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
 echo -e " Vmess Status: $status_vmess  Trojan Status: $status_trojan"
 echo -e " VLess Status: $status_vless  Nginx Status: $status_nginx"     
