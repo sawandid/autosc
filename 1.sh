9fa802287fb19568770354aa1d27befcf729fe9d
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-24 07:32:29 +0700

    Update 1.sh
    Signed-off-by: rullpqh_ <114976790+rullpqh@users.noreply.github.com>

--- 1.sh
+++ 1.sh
@@ -494,21 +494,11 @@ wget -q -O /var/www/html/index.html ${myhost_html}index.html
 cat >/etc/nginx/conf.d/$domain <<EOF
     server {
              listen 80 default_server;
-             listen [::]:80 default_server;
              listen 443 ssl http2 default_server;
-             listen [::]:443 ssl http2 default_server;
-             return 301 https://$domain$request_uri;
              server_name $domain;
              ssl_certificate /etc/xray/xray.crt;
              ssl_certificate_key /etc/xray/xray.key;
-             ssl_stapling on;
-             ssl_stapling_verify on;
-             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
-             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
-             access_log  /dev/null;
-             error_log  /dev/null;
              root /var/www/html;
-             index index.html index.htm index.php default.php default.htm default.html;
         }
 EOF
 sed -i '$ ilocation = /vless' ${nginx_conf}/$domain
