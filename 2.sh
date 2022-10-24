eb690fef62f07832b88e386c57ad034a8c989dce
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-24 07:21:25 +0700

    Update 2.sh
    Signed-off-by: rullpqh_ <114976790+rullpqh@users.noreply.github.com>

--- 2.sh
+++ 2.sh
@@ -478,16 +478,18 @@ rm /var/www/html/*.html
 wget -q -O /var/www/html/index.html ${myhost_html}index.html
 cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
-             listen 80;
-             listen [::]:80;
-             listen 443 ssl http2 reuseport;
-             listen [::]:443 http2 reuseport;	
-             server_name $domain www.$domain;
-             ssl_certificate /etc/xray/xray.crt;
-             ssl_certificate_key /etc/xray/xray.key;
-             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
-             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
              root /var/www/html;
+             listen 80 default_server;
+             listen [::]:80 default_server;
+             server_name $domain www.$domain;  
+             ssl_certificate /etc/xray/xray.crt;             
+             listen 443 ssl http2 default_server;
+             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;             
+             return 301 https://$domain$request_uri; 
+             ssl_certificate_key /etc/xray/xray.key;             
+             listen [::]:443 ssl http2 default_server;
+             index index.html index.htm index.php default.php default.htm default.html;
+             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
         }
 EOF
 sed -i '$ ilocation = /vless' /etc/nginx/conf.d/$domain.conf
