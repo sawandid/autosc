cf8c4cdd60ff55ac51799dc3d3bcfa20c9f2f455
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-24 20:39:52 +0700

    Update s.sh
    Signed-off-by: rullpqh_ <114976790+rullpqh@users.noreply.github.com>

--- s.sh
+++ s.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
-# My Telegram : https://t.me/admmoi
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-
-clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
-echo "----------------------------------------";
-echo "---------=[ Vmess User Login ]=---------";
-echo "----------------------------------------";
+data=( `cat /etc/v2ray/config.json | grep '^###' | cut -d ' ' -f 2`);
+echo "-------------------------------";
+echo "-----=[ Vmess User Login ]=-----";
+echo "-------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
 echo -n > /tmp/ipvmess.txt
-data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep v2ray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
 jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
@@ -44,13 +29,12 @@ else
 jum2=$(cat /tmp/ipvmess.txt | nl)
 echo "user : $akun";
 echo "$jum2";
-echo "----------------------------------------"
+echo "-------------------------------"
 fi
 rm -rf /tmp/ipvmess.txt
 done
 oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
-echo "----------------------------------------"
-echo "Script Mod By ADAM SIJA"
+echo "-------------------------------"
 rm -rf /tmp/other.txt
