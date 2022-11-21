45c6247e14af84f1375ca8a22053b730ac260e70
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-11-21 21:10:57 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -274,23 +274,6 @@ location = /vless
             }
 
 
-
-# Important:
-# This is the proxy Xray For Vmess Servers
-      location = /vmess
-{
-proxy_redirect off;
-proxy_pass http://127.0.0.1:8082;
-proxy_http_version 1.1;
-proxy_set_header X-Real-IP \$remote_addr;
-proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-proxy_set_header Upgrade \$http_upgrade;
-proxy_set_header Connection "upgrade";
-proxy_set_header Host \$http_host;
-}
-
-
-
 # Important:
 # This is the proxy Xray For Trojan Servers
       location = /trojan-ws
