1f93ae813ef1ceb1dac3ffacb782a85b758d915b
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-11-22 20:25:04 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -62,9 +62,9 @@ judge() {
 
 
 cloudflare() {
-    DOMEN="yha.my.id"
+    DOMEN="yha.biz.id"
     sub=$(tr </dev/urandom -dc a-z0-9 | head -c2)
-    domain="cloud-${sub}.yha.my.id"
+    domain="cloud-${sub}.yha.biz.id"
     echo -e "${domain}" >/etc/xray/domain
     CF_ID="nuryahyamuhaimin@gmail.com"
     CF_KEY="9dd2f30c099dbcf541cbd5c188d61ce060cf7"
