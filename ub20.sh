1fa391fd811ea96a5736826195768910ab740879
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-11-26 02:14:33 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -464,7 +464,7 @@ RAM          : <code>${RAMMS}MB</code>
 LINUX       : <code>${OS}</code>
 "
     curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
-    sed -i "s/xxx/${domain}/g" /var/www/html/templates/index.html >/dev/null 2>&1
+    sed -i "s/xxx/${domain}/g" /var/www/html/index.html >/dev/null 2>&1
     sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/xray.conf >/dev/null 2>&1
     sed -i -e 's/\r$//' /usr/bin/get-backres >/dev/null 2>&1
     systemctl daemon-reload >/dev/null 2>&1
